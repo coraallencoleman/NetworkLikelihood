18314282e13cbf76fc1501f64116a397b02c3efa
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2018-10-15 15:44:14 -0500

    working compareGraphs function

--- computational_networks/compareGraphs.R
+++ computational_networks/compareGraphs.R
@@ -14,7 +14,8 @@ setwd("/Users/cora/git_repos/NetworkLikelihood/computational_networks")
 graph1txt <- "factors_net.txt"
 graph2txt <- "factors_net_predicted.txt" #added one dir edge, lost one dir edge
 
-compareGraphs <- function(graph1, graph2, directed){
+compareGraphs(graph1txt, graph2txt)
+compareGraphs <- function(graph1txt, graph2txt, directed){
   # Compare 2 graphs using precision, recall and F-score.
   # Args:
   #   graph1 = true graph txt file name as string
@@ -31,39 +32,19 @@ compareGraphs <- function(graph1, graph2, directed){
   # Dependencies:
   graph1 <- read.table(graph1txt, stringsAsFactors = F)
   graph2 <- read.table(graph2txt, stringsAsFactors = F)
-  graph1$V1 <- as.numeric(graph1$V1)
-  graph1$V2 <- as.numeric(graph1$V2)
-  
-  graph2$V1 <- as.numeric(graph2$V1)
-  graph2$V2 <- as.numeric(graph2$V2)
-  
-  #order
-  graph1 <- graph1[order(graph1[,1], graph1[,2]), ]
-  graph2 <- graph2[order(graph2[,1], graph2[,2]), ]
   
   #compare each line
   predictedEdges <- nrow(graph2)
   trueEdges <- nrow(graph1)
-  correctEdges <- trueEdges - nrow(setdiff(graph2, graph2))
-  correctEdges = 0
-  for (i in 1:nrow(graph1)){
-    #print(length(graph2$V1 == graph1[i,]$V1 && graph2$V2 == graph1[i,]$V2))
-    setdiff(graph1,graph2)
-    if(length(graph2$V1 == graph1[i,]$V1 && graph2$V2 == graph1[i,]$V2) > 0){
-      correctEdges = correctEdges + 1
-    }
-    setdiff(graph1, graph2)
-  }
-  
+  correctEdges <- trueEdges - nrow(setdiff(graph2, graph1))
 
   precision <- correctEdges/predictedEdges # correct edges/ # predicted edges
 
   recall <- correctEdges/trueEdges # correct edges/ # true edges
 
   #F-score
-  fscore <- 
+  fscore <- (2*precision*recall)/(precision + recall)
 
   return(paste("Precision: ", precision, " Recall: ", recall, " F-score: ", fscore))
 }
-
 #compareGraphs(args)
